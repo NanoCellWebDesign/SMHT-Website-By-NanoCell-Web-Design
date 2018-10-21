30005067949b201559f0828f23635d9a601eee42
NanoCell Web Design <43909932+NanoCellWebDesign@users.noreply.github.com>
2018-10-21 22:39:43 +0100

    Edits

--- php/db_conn.inc.php
+++ php/db_conn.inc.php
@@ -1,14 +1,15 @@
 <?php
-$user = 'root';
-$password = 'root';
-$db = 'smht';
-$host = 'localhost';
-$port = 3306;
+$servername = "mysql.hostinger.co.uk";
+$database = "u209710757_ncwd";
+$username = "u209710757_nano";
+$password = "******";
 
-$conn = mysqli_connect($host, $user, $password, $db);
+// Create connection
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
 if (!$conn) {
 
-  die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 
 }
-?>
--- php/db_conn_mysqli.inc.php
+++ php/db_conn_mysqli.inc.php
@@ -1,11 +1,9 @@
 <?php
-$user = 'root';
-$password = 'root';
-$db = 'smht';
-$host = 'localhost';
-$port = 3306;
-
+$servername = "mysql.hostinger.co.uk";
+$database = "u209710757_ncwd";
+$username = "u209710757_nano";
+$password = "*********";
 
 // Create connection
 
-$mysqli= new mysqli($host, $user, $password, $db);
+$mysqli= new mysqli($servername, $username, $password, $database);
